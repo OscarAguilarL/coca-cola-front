e5cf73761fe54f12b4396f9138cb5be1d1eed233
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-10-22 23:13:28 -0500

    base layout

---
+++ src/vue-app/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+      <router-link to="/" class="navbar-brand">Coca-Cola</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarColor01"
+        aria-controls="navbarColor01"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarColor01">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <router-link to="/" exact class="nav-link" active-class="active">
+              Inicio
+              <span class="visually-hidden">(current)</span>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/contact" class="nav-link" active-class="active">
+              Contact
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({ name: 'Navbar' })
+export default class Navbar extends Vue {}
+</script>
---
+++ src/vue-app/layout/BaseLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({ name: 'BaseLayout' })
+export default class BaseLayout extends Vue {}
+</script>
